dc05ce7c2ae636c392cbbcdb895d49b608610c0a
Rushtell <ultematum091@gmail.com>
2021-04-15 11:49:19 +0500

    Change MainPoint in Page

--- ApiRushtellSite/App.xaml.cs
+++ ApiRushtellSite/App.xaml.cs
@@ -13,5 +13,18 @@ namespace ApiRushtellSite
     /// </summary>
     public partial class App : Application
     {
+        //App()
+        //{
+        //    InitializeComponent();
+        //}
+
+        [STAThread]
+        static void Main()
+        {
+            MainWindow view = new MainWindow();
+
+            App app = new App();
+            app.Run(view);
+        }
     }
 }
